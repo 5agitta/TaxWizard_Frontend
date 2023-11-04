f5ae74a8b9c26af2ef2d5b40188c4a5a5b1b73d3
acesuu <ahnafakib@iut-dhaka.edu>
2023-11-04 07:11:17 +0600

    added golbal axios for authorization

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    const authToken = localStorage.getItem("authToken");
+    axios.defaults.headers.common['Authorization'] = `Bearer: ${authToken}`;
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
